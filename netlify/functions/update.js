e432cc859e312eac7c348d55dde8a6b2b2a50ca2
Yasaman Karbasi <work.yasaman.me@gmail.com>
2024-08-02 20:58:24 +0330

    init

---
+++ netlify/functions/update.js
@@ -0,0 +1,13 @@
+const sendMessage = require('../../sendMessage');
+
+exports.handler = async (event) => {
+  try {
+    const { message } = JSON.parse(event.body);
+    const chat_id = message.chat.id;
+    await sendMessage(chat_id, 'hi');
+    return { statusCode: 200, body: '' };
+  } catch (error) {
+    console.error('error in handler:', error);
+    return { statusCode: 400, body: 'This endpoint is meant for bot and telegram communication' };
+  }
+};
